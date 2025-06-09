4d79866dc66a81354f087105bbac2967db53cf63
Guang1985 <wguangzhao85@gmail.com>
2025-06-09 23:12:35 +0800

    Update q3.py

--- src/q3.py
+++ src/q3.py
@@ -1,4 +1,16 @@
 def update_dictionary(dct, key, value):
+
+    #Check if dct is a dictionary
+    if not isinstance (dct, dict):
+        return "Error: First argument must be a dictionary"
+
+    #if the key already exists, print the original value
+    if key in dct:
+        print(f"Original value for '{key}': {dct[key]}")
+
+    #Update or add the key-value pair
+    dct[key] = value
+    return dct
     """
     Task 1
     - Create a function that updates a dictionary (dct) with a new key-value pair.
